897445206382747b078b492f7351973be9a68386
Garry Aylott <gazaylott@gmail.com>
2016-12-11 22:17:13 +0000

    Page format and header bg styles.

--- page.php
+++ page.php
@@ -1,28 +1,17 @@
-<?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package Big Blue Box
- */
+<?php get_header('page'); ?>
 
-get_header(); ?>
+<!-- Start main content -->
+<section class="main-content">
 
-	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
 
-			<?php endwhile; // end of the loop. ?>
+			<?php endwhile; ?>
+		</main>
+		<!-- #main -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
+</section>
+<!-- End main content -->
 <?php get_footer(); ?>
