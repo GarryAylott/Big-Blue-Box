1f42835ec51e5b6b702a2f134f7aa3a87ee66956
Garry Aylott <gazaylott@gmail.com>
2017-09-28 17:19:07 +0100

    Styled header H1 text, tweaked line-hights and removed show notes link from non-audio post headers.

--- content-header_home.php
+++ content-header_home.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('header-latest-post'); ?> style="background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>'); background-size: cover; background-position: 0 30%; background-repeat: no-repeat;">
+<article id="post-<?php the_ID(); ?>" <?php post_class('header-latest-post'); ?> style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>'); background-size: cover; background-position: 0 30%; background-repeat: no-repeat;">
 
 	<section class="inner">
 		<div class="header-audio--post-content">
