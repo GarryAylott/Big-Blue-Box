134b69b5ec88953876c352c54e8e1d75dc4b8e16
Garry Aylott <gazaylott@gmail.com>
2017-11-11 20:37:59 +0000

    Social sharing buttons added to posts.

--- content-single.php
+++ content-single.php
@@ -6,6 +6,7 @@
 
 	<div class="entry-content">
 		<?php the_content(); ?>
+		<?php echo do_shortcode('[easy-social-share ukey="1510432172"]'); ?> 
 		<div class="fb-comments">
 			<?php echo do_shortcode('[fbcomments count="off" num="5" width="100%"]'); ?>
 		</div>
