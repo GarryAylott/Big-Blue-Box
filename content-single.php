95500a87c3b4eeb119e6b7a95a08c3b7bc651a48
Garry Aylott <gazaylott@gmail.com>
2017-11-11 21:25:23 +0000

    Social sharing width and placement update v4.

--- content-single.php
+++ content-single.php
@@ -7,7 +7,11 @@
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<div class="post-share-btns">
-			<?php echo do_shortcode('[easy-social-share ukey="1510432172"]'); ?>
+		<?php 
+			$url = get_permalink($post->ID);
+			$title = get_the_title($post->ID);
+			echo do_shortcode('[easy-social-share buttons="facebook,twitter,google,pinterest,mail,reddit" counters=1 counter_pos="hidden" total_counter_pos="left" style="button" message="yes" template="59" point_type="simple" url="'.$url.'" text="'.$title.'"]'); 
+		?> 
 		</div>
 		<div class="fb-comments">
 			<?php echo do_shortcode('[fbcomments count="off" num="5" width="100%"]'); ?>
