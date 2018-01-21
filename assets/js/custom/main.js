c003e3dba7904bd3e94f99a080aac62c7fdea96d
Garry Aylott <gazaylott@gmail.com>
2018-01-21 17:38:02 +0000

    Further theme base files tidy up plus addition of sass, images etc.

---
+++ assets/js/custom/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	// Add rel noopener etc
+	$('a[target^="_blank"]').attr('rel','noopener noreferrer');
+});
--- functions.php
+++ functions.php
@@ -119,20 +119,13 @@ add_action( 'widgets_init', 'bigbluebox_widgets_init' );
 function bigbluebox_scripts() {
 	wp_enqueue_style( 'bigbluebox-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'bigbluebox-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'bigbluebox-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	// wp_enqueue_script( 'bigbluebox-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'bigbluebox_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
@@ -143,11 +136,6 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
